d7de6eae402290227d8ea6a7bc6f5bd6829bee6d
softcup <softcup@gmail.com>
2010-01-30 17:07:03 +0800

    小調整程式，加上錯誤判斷

--- rePlurk.js
+++ rePlurk.js
@@ -4,7 +4,7 @@
  **/
 var rePlurk = function (doc) {
 	var
-		version   = 0.1,
+		version   = 0.1.1,
 		include   = ["http://www.plurk.com/"],
 		exclude   = ["http://www.plurk.com/_comet/", "http://www.plurk.com/User/", "http://www.plurk.com/i18n/"],
 		tabDoc = jetpack.tabs.focused.contentDocument
@@ -38,15 +38,14 @@ var rePlurk = function (doc) {
 		o_expand(div);
 
 		var ib = $(tabDoc).find("#input_big");
-		if (ib.length > 0) {
+		if ( (ib.length > 0) && (typeof tabJs.getPD == "function") ) {
 			var plurk = tabJs.getPD(div).obj;
 			var link = "http://plurk.com/p/" + (plurk.plurk_id).toString(36);
 			var raw = plurk.content_raw;
 			var owner_id = plurk.owner_id;
 
 			var pl = $(tabDoc).find("div.info_box div.perma_link");
-			if (pl.length > 0) {
-				if ($(tabDoc).find("div.info_box a.RePlurk").length > 0) return;
+			if ( (pl.length > 0) && ($(tabDoc).find("div.info_box a.RePlurk").length <= 0) ) {
 				pl.after('<a href="#" style="float: right;" class="RePlurk">轉噗</a>');
 				$(tabDoc).find("div.info_box a.RePlurk").click(function () {
 					doRePlurk(owner_id, raw, link);
