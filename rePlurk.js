89cf611f6f5d36a7460992b056c410230e744224
unknown <Administrator@.(none)>
2010-07-26 11:27:38 +0800

    Fix can not replurk

--- rePlurk.js
+++ rePlurk.js
@@ -1,10 +1,22 @@
 /**
- * 程式參考：RePlurk (http://userscripts.org/scripts/show/39186)
+ * Reference code：RePlurk (http://userscripts.org/scripts/show/39186)
  * softcup
+ * http://jetpackgallery.mozillalabs.com/jetpacks/325
  **/
+var manifest = {
+	settings: [{
+		name: "replurk",
+		type: "text",
+		label: '<span style="white-space:nowrap;">轉噗文字</span>',
+		default: "%link% ([原噗]) %nickname% %plurkcontent%"
+	}]
+};
+
+jetpack.future.import("storage.settings");
+
 var rePlurk = function (doc) {
 	var
-		version   = "0.1.2",
+		version   = "0.1.4.1",
 		include   = ["http://www.plurk.com/"],
 		exclude   = ["http://www.plurk.com/_comet/", "http://www.plurk.com/User/", "http://www.plurk.com/i18n/"],
 		tabDoc    = jetpack.tabs.focused.contentDocument,
@@ -24,8 +36,10 @@ var rePlurk = function (doc) {
 		if (typeof tabJs.Plurks == "undefined") return;
 
 		var nick = tabJs.SiteState.getUserById(owner_id).nick_name;
+		var txt  = jetpack.storage.settings.replurk;
+		txt = txt.replace("%nickname%", (nick ? ("@" + nick + ": ") : "") ).replace("%link%", link).replace("%plurkcontent%", raw);
 
-		$(tabDoc).find("#input_big").val(link + " ([原噗]) " + ((nick) ? ("@" + nick + ": ") : "") + raw);
+		$(tabDoc).find("#input_big").val(txt);
 		tabJs.Plurks._removeExpand();
 		tabJs.MaxChar.updateBig();		
 	};
@@ -40,14 +54,17 @@ var rePlurk = function (doc) {
 		var ib = $(tabDoc).find("#input_big");
 		if ( (ib.length > 0) && (typeof tabJs.getPD == "function") ) {
 			var plurk = tabJs.getPD(div).obj;
+			// jetpack.notifications.show("id: " + plurk.id);
 			var link = "http://plurk.com/p/" + (plurk.plurk_id).toString(36);
 			var raw = plurk.content_raw;
 			var owner_id = plurk.owner_id;
 
 			var pl = $(tabDoc).find("div.info_box div.perma_link");
-			if ( (pl.length > 0) && ($(tabDoc).find("div.info_box a.RePlurk").length <= 0) ) {
-				pl.after('<a href="#" style="float: right;" class="RePlurk">轉噗</a>');
-				$(tabDoc).find("div.info_box a.RePlurk").click(function () {
+			if (pl.length > 0) {
+				if ($(tabDoc).find("div.info_box a.RePlurk").length <= 0) {
+					pl.after('<a href="#" style="float: right;" class="RePlurk">轉噗</a>');
+				}
+				$(tabDoc).find("div.info_box a.RePlurk").unbind("click").click(function () {
 					doRePlurk(owner_id, raw, link);
 				});
 			}
